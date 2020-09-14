a25d9b895ce3f0fa803ad22f7d5fd87a40653f69
deven <devjob.2012@gmail.com>
2020-09-14 11:56:55 +0100

    Renamed

---
+++ First.py
@@ -0,0 +1,19 @@
+print('Hello World')
+b = '23'
+b  =  198;
+
+c =  2 * b;
+print('Value of  c is ' ,  c);
+
+stuff = ["Hello, World!", "Hi there, Everyone!", '6']
+for i in stuff:
+    print(i + " Foobar!")
+print(b)
+print ('Hello World ------ > ')
+x = 4 # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
